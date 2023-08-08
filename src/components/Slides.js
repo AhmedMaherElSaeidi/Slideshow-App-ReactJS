b928d34512d9d42efa2ac9d564adf85e5f553eb2
Ahmed Maher <ahmedelseaidy22@gmail.com>
2023-08-08 05:15:16 +0300

    first commit

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Slides({slides}) {
+function Slides({ slides }) {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
+  const goToNextSlide = () => {
+    setCurrentSlideIndex((prevIndex) => (prevIndex === slides.length - 1 ? 0 : prevIndex + 1));
+  };
 
+  const goToPrevSlide = () => {
+    setCurrentSlideIndex((prevIndex) => (prevIndex === 0 ? slides.length - 1 : prevIndex - 1));
+  };
+
+  const goToFirstSlide = () => {
+    setCurrentSlideIndex(0);
+  };
+
+  const currentSlide = slides[currentSlideIndex];
+
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          onClick={goToFirstSlide}
+          disabled={currentSlideIndex === 0}
+        >
+          Restart
+        </button>
+        <button
+          data-testid="button-prev"
+          className="small"
+          onClick={goToPrevSlide}
+          disabled={currentSlideIndex === 0}
+        >
+          Prev
+        </button>
+        <button
+          data-testid="button-next"
+          className="small"
+          onClick={goToNextSlide}
+          disabled={currentSlideIndex === slides.length - 1}
+        >
+          Next
+        </button>
+      </div>
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title">{currentSlide.title}</h1>
+        <p data-testid="text">{currentSlide.text}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Slides;
